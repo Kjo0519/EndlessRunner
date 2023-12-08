0b23b77bc2dbb2b1bfdad051499208e4a37831cb
STEMPLE0475 <stemple0@naver.com>
2023-12-08 17:49:45 +0900

    2023.12.08 - 5:48
    Stage02 추가, Stage02 플레이어 컨트롤 구현, 플레이어 잔상 이펙트 실험 중

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+using System;
+using UnityEditor.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int stage = 1;
+    float score = 0;
+    public GameObject player;
+    public GameObject ccamera;
+    public GameObject stage02;
+    public GameObject stage01;
+    TextMeshProUGUI scoreText;
+
+    bool isTwoStage = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+        stage02 = GameObject.Find("Stage02");
+        stage01 = GameObject.Find("Stage01");
+        stage02.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        score += Time.deltaTime;
+        scoreText.text = "Score : " + Math.Ceiling(score).ToString();
+        
+        
+        
+        
+        Stage();
+        if(stage == 2 && !isTwoStage)
+        {
+            ccamera.transform.position = new Vector3(ccamera.transform.position.x, -25, ccamera.transform.position.z);
+            player.transform.position = player.transform.position + new Vector3(0, -25, 0);
+            stage02.SetActive(true);
+            stage01.SetActive(false);
+            isTwoStage = true;
+        }
+
+    }
+
+    void Stage()
+    {
+        if (score <= 20)
+        {
+            stage = 1;
+        }
+        else if (score <= 40)
+        {
+            stage = 2;
+        }
+        else
+        {
+            stage = 2;
+        }
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     Rigidbody2D rb;
     float gravityForce = 13f;
-    float jumpForce = 400f;
+    float jumpForce = 800f;
+    GameManager GM;
+    int stage;
+    bool moveUp;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
         Physics2D.gravity = new Vector2(0, -gravityForce);
         rb = GetComponent<Rigidbody2D>();
     }
@@ -18,10 +24,31 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (GM.stage == 1)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
-            Debug.Log("Jump");
+            if (Input.GetMouseButtonUp(0))
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+                Debug.Log("Jump");
+            }
         }
+        else if (GM.stage >= 2)
+        {
+            Physics2D.gravity = Vector2.zero;
+            if (moveUp)
+            {
+                rb.velocity = Vector2.up * 2000f * Time.deltaTime;
+            }else if (!moveUp)
+            {
+                rb.velocity = Vector2.up * -2000f * Time.deltaTime;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                moveUp = !moveUp;
+                
+            }
+        }
+        
     }
 }
